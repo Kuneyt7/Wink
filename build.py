c5c6f7d31dd81c1f420658a174c60858fa38ace6
Ryekun <59950546+Ryekun1@users.noreply.github.com>
2025-03-28 00:47:11 -0400

    velix build.py

--- build.py
+++ build.py
@@ -1,17 +1,17 @@
-hbb_name = 'blocklink' + ('.exe' if windows else '')
+hbb_name = 'velixos' + ('.exe' if windows else '')
 exe_path = 'target/release/' + hbb_name
 
 def generate_control_file(version):
     control_file_path = "../res/DEBIAN/control"
     system2('/bin/rm -rf %s' % control_file_path)
 
-    content = """Package: blocklink
+    content = """Package: velixos
 Section: net
 Priority: optional
 Version: %s
 Architecture: %s
-Maintainer: blocklink <info@blocklink.com>
-Homepage: https://blocklink.com
+Maintainer: velix os <info@velixos.com>
+Homepage: https://velixos.com
 Depends: libgtk-3-0, libxcb-randr0, libxdo3, libxfixes3, libxcb-shape0, libxcb-xfixes0, libasound2, libsystemd0, curl, libva2, libva-drm2, libva-x11-2, libgstreamer-plugins-base1.0-0, libpam0g, gstreamer1.0-pipewire%s
 Recommends: libayatana-appindicator3-1
 Description: A remote control software.
